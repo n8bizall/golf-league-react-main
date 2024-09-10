46a7e830a731b58ea701d844f5f1db6678df3cfb
tiffa <tiffa@DESKTOP-BK5JU5E>
2024-09-10 12:56:26 -0400

    env

--- src/firebase.js
+++ src/firebase.js
@@ -10,14 +10,14 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBdfqp1Ov90Hoibv17X-Ue79jdJk4FyhiI",
-  authDomain: "golfleague2025.firebaseapp.com",
-  databaseURL: "https://golfleague2025-default-rtdb.firebaseio.com",
-  projectId: "golfleague2025",
-  storageBucket: "golfleague2025.appspot.com",
-  messagingSenderId: "575574407684",
-  appId: "1:575574407684:web:cc14ae21ab82aac9b0d351",
-  measurementId: "G-DJXHVZFSBG",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
